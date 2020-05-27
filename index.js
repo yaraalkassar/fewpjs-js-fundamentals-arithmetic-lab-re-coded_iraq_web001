ab3e012a0a15e1dab34b85bca1a6531a8deaba30
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-27 06:56:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply= num1*num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 24%5;
+let max = Math.max(5, 8, 9, 16, 20);
